b7829b40629bb319ca09cc1ba4fef694892f7c8d
ptrierweiler <ptrierweiler@gmail.com>
2016-10-29 20:09:14 -0400

    Added user bot and added pydate format

--- Edison42Bot.py
+++ Edison42Bot.py
@@ -2,11 +2,17 @@ import logging
 import os
 import psycopg2
 from telegram.ext import Updater
+from datetime import datetime
 
 # reading token from token file
 token_file = '/scripts/bots/Edison42bot_token.txt'
 with open(token_file, 'r', encoding='utf-8') as f:
-    tok =f.readline()
+    tok = f.readline()
+
+# bot password
+pass_file = '/scripts/bots/bot_pass.txt'
+with open(token_file, 'r', encoding='utf-8') as f:
+    bot_pass = f.readline()
 
 updater = Updater(token=tok)
 dispatcher = updater.dispatcher
@@ -39,16 +45,21 @@ dispatcher.add_handler(prcp_handler)
 
 
 def prcp_srch_3(pt_id, std, etd):
-	conn = psycopg2.connect("dbname='fato'")
+	# converting date strings to python datetime
+	pyd_std = datetime.strptime(std, '%Y-%m-%d')
+	pyd_etd = datetime.strptime(etd, '%Y-%m-%d')
+	conn = psycopg2.connect("dbname='fato' host='localhost' user='bot' password='{pas}'".format(pas=bot_pass))
 	cur = conn.cursor()
-	cur.execute("select date||': '||mean as output from cpc_glb_dly_prec.data \
-	where pt_id = {pt_id} and date >= '{std}' and date <= '{etd} order by date'".format(
-	pt_id=int(pt_id), std=std, etd=etd))
+	cur.execute("select date||': '||mean||' : '||median as output from cpc_glb_dly_prec.data \
+	where pt_id = {pt_id} and date >= '{std}' and date <= '{etd}' order by date".format(
+	pt_id=int(pt_id), std=pyd_std, etd=pyd_etd))
 	db_out = cur.fetchall()
-	out_text = ""
+	out_text = "date, mean, median"
 	for i in db_out:
 		dt = i[0]
 		out_text = out_text + dt + '\n'
+	cur.close()
+	conn.close()
 	return out_text
 	
 
