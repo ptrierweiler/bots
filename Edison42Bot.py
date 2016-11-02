9f9ddc2780d707eb718457d166e98ed9a0fe2dfa
ptrierweiler <ptrierweiler@gmail.com>
2016-11-02 09:55:29 -0400

    Added fuction to read in text files

--- Edison42Bot.py
+++ Edison42Bot.py
@@ -3,7 +3,7 @@ import logging
 import os
 import psycopg2
 from telegram.ext import Updater
-from datetime import datetime
+from datetime import datetime, timedelta
 
 # reading token from token file
 token_file = '/scripts/bots/Edison42bot_token.txt'
@@ -18,19 +18,35 @@ with open(pass_file, 'r', encoding='utf-8') as f:
 updater = Updater(token=tok)
 dispatcher = updater.dispatcher
 
+def text_loader(text):
+    path = "/scripts/bots/texts/"
+    textpath = path + text
+    with open(textpath, 'r', encoding='utf-8') as f:
+        out_text = f.read()
+    return(out_text)
+	
 # logging
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
 
-start_text = "Hello I am Edison\n\
-I can help find weather data\n\n\
-Please use the following commands:\n\n\
-/pcrp - for precipitation data"
+#start_text = "Hello I am Edison\n\
+#I can help find weather data\n\n\
+#Please use the following commands:\n\n\
+#/help - for help\n\
+#/pcrp - for precipitation data"
+
+start_text = text_loader("start.txt")
+
 
 def start(bot, update):
    bot.sendMessage(chat_id=update.message.chat_id, text=start_text)
+
+def help(bot,update):
+    bot.sendMessage(chat_id=update.message.chat_id, text=help_text)
+    
    
 from telegram.ext import CommandHandler
 start_handler = CommandHandler('start', start)
+help_handler = CommandHandler('help', help)
 dispatcher.add_handler(start_handler)
 updater.start_polling()
 
@@ -41,28 +57,48 @@ def prcp(bot, update, args):
         data_text = prcp_srch_3(arg_list[0], arg_list[1], arg_list[2])
     bot.sendMessage(chat_id=update.message.chat_id, text=data_text)
 
+
+def prcp_srch_3(pt_id,_std, etd):
+	# converting date strings to python datetime
+    pyd_std = datetime.strptime(std, '%Y-%m-%d')
+    pyd_etd = datetime.strptime(etd, '%Y-%m-%d')
+    conn = psycopg2.connect("dbname='fato' host='localhost' user='bot' password='{pas}'".format(pas=bot_pass))
+    cur = conn.cursor()
+    cur.execute("select date, mean, median as output from cpc_glb_dly_prec.data \
+    where pt_id = {pt_id} and date >= '{std}' and date <= '{etd}' order by date".format(
+    pt_id=int(pt_id), std=pyd_std, etd=pyd_etd))
+    db_out = cur.fetchall()
+    out_text = "\t date mean median\n"
+    for i in db_out:
+        dt = i[0]
+        mn = i[1]
+        md = i[2]
+        out_text = out_text + "{dt}: {mn} : {md}\n".format(dt=dt, mn=mn, md=md)
+    cur.close()
+    conn.close()
+    return out_text
+	
+
+def prcp_chart(sql_out,geo):
+    date = []
+    mean = []
+    med = []
+    for i in sql_out:
+        date.append(i[0])
+        mean.append(i[1])
+        med.append(i[2])
+    # adding dummy dates
+    date.insert(0,min(date) - timedelta(1))
+    date.insert(len(date)+ 1, max(date) + timedelta(1))
+    # adding dummy values
+    mean.insert(0,None)
+    mean.insert(len(med + 1),None)
+    plt.plot_date(pydates, mean, fmt='bo', tz=None, xdate=True)
+    plt.xticks(pydates)
+    plt.xticks(rotation=80)
+    plt.tight_layout()
+    plt.ylabel('PRCP mm')
+    plt.xlabel('Dates')
+
 prcp_handler = CommandHandler('prcp', prcp, pass_args=True)
 dispatcher.add_handler(prcp_handler)
-
-
-def prcp_srch_3(pt_id, std, etd):
-	# converting date strings to python datetime
-	pyd_std = datetime.strptime(std, '%Y-%m-%d')
-	pyd_etd = datetime.strptime(etd, '%Y-%m-%d')
-	conn = psycopg2.connect("dbname='fato' host='localhost' user='bot' password='{pas}'".format(pas=bot_pass))
-	cur = conn.cursor()
-	cur.execute("select date||': '||mean||' : '||median as output from cpc_glb_dly_prec.data \
-	where pt_id = {pt_id} and date >= '{std}' and date <= '{etd}' order by date".format(
-	pt_id=int(pt_id), std=pyd_std, etd=pyd_etd))
-	db_out = cur.fetchall()
-	out_text = "date, mean, median\n"
-	for i in db_out:
-		dt = i[0]
-		out_text = out_text + dt + '\n'
-	cur.close()
-	conn.close()
-	return out_text
-	
-
-	
-	
